de6f58ae5a3acfabafe8937857c33d176cb98fc0
beepscore <support@beepscore.com>
2017-10-04 10:07:40 -0700

    Add PushNotificationController

---
+++ MockPlay/PushNotificationController.swift
@@ -0,0 +1,16 @@
+//
+//  PushNotificationController.swift
+//  MockPlay
+//
+//  Created by Steve Baker on 10/4/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import Foundation
+
+
+/// Reference: MocksPlayground.swift
+/// https://gist.github.com/eliperkins/8f4115151497dc1953ea
+struct PushNotificationController {
+
+}
