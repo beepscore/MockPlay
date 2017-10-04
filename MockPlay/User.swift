5b635e06d0e8897176a76f79a1e56ed2dd4a0d8a
beepscore <support@beepscore.com>
2017-10-04 10:23:46 -0700

    Add User.swift

---
+++ MockPlay/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  MockPlay
+//
+//  Created by Steve Baker on 10/4/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import Foundation
+
+/// A minimal struct, just enough to be able to instantiate
+struct User {
+}
